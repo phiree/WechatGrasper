80d61ef1f0b00f0b98b2cc330a82b7774cd651f9
YuanFei <YuanFei@DESKTOP-FI2RCSQ>
2020-06-22 18:14:49 +0800

    template

---
+++ Domain/Application/WHY/WhyActivityRequest.cs
@@ -0,0 +1,39 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using TourInfo.Domain.DomainModel;
+
+namespace TourInfo.Domain.Application.WHY
+{
+    public class WhyActivityRequest:IRequestWithPaging
+    {
+        
+        public int currentPage { get; set; }
+        public string categoryId { get; set; }
+        public string areaId { get; set; }
+        public string villageId { get; set; }
+        public int lineSize { get; set; }
+        public int PageIndex { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int PageSize { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public IRequestWithPaging BuildNextPageRequest()
+        { 
+             currentPage+=1;
+            return this;
+            }
+        public IList<Parameter> CreateParameters()
+        { 
+            return new List<Parameter> { 
+                new Parameter("currentPage",currentPage, ParameterType.GetOrPost),
+                new Parameter("categoryId",categoryId, ParameterType.GetOrPost),
+                new Parameter("areaId",areaId, ParameterType.GetOrPost),
+                new Parameter("villageId",villageId, ParameterType.GetOrPost),
+                 new Parameter("lineSize",lineSize, ParameterType.GetOrPost),
+                };
+            }
+
+        
+    }
+}
